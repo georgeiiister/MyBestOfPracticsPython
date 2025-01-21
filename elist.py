5f09b8591518c024a826e243598c6489f9a55118
georgeiiister <georgeiiister@gmail.com>
2025-01-21 07:12:13 +0300

    refactoring

--- elist.py
+++ elist.py
@@ -1,20 +1,31 @@
 class EList:
-    def __init__(self,obj=None):
+    def __add_item(self, index:int | None, value):
+        """add value by index"""
+        if index is not None:
+            if index in self.__values:
+                self.__values[index] = value
+            else:
+                raise IndexError
+        else:
+            try:
+                for j, value in enumerate(value):
+                    self.__values[j] = value
+            except TypeError:
+                pass
+
+    def __init__(self, value = None):
         self.__values = {}
-        try:
-            for j, value in enumerate(obj):
-                self.__values[j] = value
-        except TypeError:
-            pass
+        self.__add_item(index = None, value = value)
 
-    def __getitem__(self, item):
-        if item in self.__values:
-            return self.__values.get(item)
-        else:
-            raise IndexError
+    def __getitem__(self, index):
+        return self.__values.get(index)
 
-    def __setitem__(self, item, other):
-        if item in self.__values:
-            self.__values[item] = other
-        else:
-            raise IndexError
+    def __setitem__(self, index, value):
+        self.__add_item(index = index, value = value)
+
+    def __len__(self):
+        return len(self.__values)
+
+    def __str__(self):
+        return f'<{",".join([i for i in self.__values.values()])}>'
+
