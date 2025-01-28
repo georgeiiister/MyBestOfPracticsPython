da97a4c3dedbe585aab15e444f81dbbc6a771923
georgeiiister <georgeiiister@gmail.com>
2025-01-28 18:40:23 +0300

    refactoring

--- sq_lite.py
+++ sq_lite.py
@@ -22,12 +22,32 @@ class Seq:
 
 class User:
     __table = 'Users'
-    __create_table_sql = f'create table if not exists {__table} (id integer, username text, email text, age integer)'
+    __create_table_sql = (
+                            f'create table if not exists {__table}'
+                            f'('
+                            f'id integer,'
+                            f'fusername text,'
+                            f'email text,'
+                            f'age integer)'
+                         )
+
     __insert_sql = f'insert into {__table} values(?,?,?,?)'
     __select_sql = f'select * from {__table} where id=?'
     __update_sql = f'update {__table} set username=?,email=?, age=? where id=?'
+    __select_avg_age_sql = f'select cast(avg(age) as integer) as av from Users'
+    __select_greatest_avg_age = f'select * from Users where age>?'
 
     __seq = Seq()
+
+    @classmethod
+    def avg_age(cls,cursor):
+        return cursor.execute(User.__select_avg_age_sql).fetchone()[0]
+
+    @classmethod
+    def result_set_greates_avg_age(cls,cursor):
+        result = cursor.execute(User.__select_greatest_avg_age,(User.avg_age(cursor = cursor),)).fetchall()
+        return {rec[0]:{'id':rec[0],'username':rec[1], 'email':rec[2], 'age':rec[3]}
+                for rec in result}
 
     @classmethod
     def create_table(cls,cursor):
@@ -78,7 +98,6 @@ class User:
     def id(self, other):
         self.__id = other
 
-
     def __select(self):
         return self.__cursor.execute(User.__select_sql, (self.__id,)).fetchall()
 
@@ -86,13 +105,19 @@ class User:
        if self.__cursor is not None:
            self.__cursor.execute(
                                    User.__update_sql,
-                                   (self.__username, self.__email, self.__age, self.__id)
+                                   (self.__username,
+                                    self.__email,
+                                    self.__age,
+                                    self.__id)
                                  )
     def __insert(self):
         if self.__cursor is not None:
             self.__cursor.execute(
                                     User.__insert_sql,
-                                    (self.__id, self.__username, self.__email, self.__age)
+                                    (self.__id,
+                                     self.__username,
+                                     self.__email,
+                                     self.__age)
                                   )
     @property
     def exists(self):
@@ -115,25 +140,87 @@ class User:
                     f'age={self.__age}'
                 )
 
-connect = sqlite3.connect('users.db')
-cur1 = connect.cursor()
-User.create_table(cursor = cur1)
+    @property
+    def cursor(self):
+        return self.__cursor
 
-usr = [User(
-             username = "".join(random.sample(string.ascii_letters, k=10)),
-             email = f'{"".join(random.sample(string.ascii_letters, k=10))}@mail.ru',
-             age = 43,
-             cursor = cur1
-           ) for i in range(10)
-      ]
+    @cursor.setter
+    def cursor(self, other):
+        self.__cursor = other
 
-for j, i in enumerate(usr):
-    i.save()
-    if j%1000 == 0:
-        connect.commit()
-connect.commit()
+class SQEnv():
+    __db = 'users.db'
+    __seq_cursors = Seq()
+    def __init__(self, db_name = __db):
+        self.__db_name = db_name
+        self.__connect = None
+        self.__cursors = []
 
-usr1 = User(id=9,cursor = cur1)
+    def __enter__(self):
+        self.__connect = sqlite3.connect(self.__db_name)
+        self.__cursor()
+        return self
 
-print(usr1)
+    def __exit__(self, exc_type = None, exc_val = None, exc_tb = None):
+        self.__cursors.clear()
+        self.__connect.close()
+        self.__connect = None
 
+    def __cursor(self):
+        cursor_id = next(SQEnv.__seq_cursors)
+        cursor_object = self.__connect.cursor()
+        self.__cursors.append((cursor_id, cursor_object))
+        return (cursor_id, cursor_object)
+
+    @property
+    def cursor(self):
+        if not self.__cursors:
+            self.__cursors()
+        return self.__cursors[0][1]    #simple get cursor - last (one) cursor
+
+    @cursor.setter
+    def cursor(self, other):
+        self.__cursors[0][1] = other
+    def test_conect(self):
+        return bool(self.cursor.execute('select "saccess"').fetchone())
+
+    def execute_any_sql(self,sql,*args):
+        self.cursor.execute(sql,args).fetchone()
+
+    def commit(self):
+        for i, cursor in self.__cursors:
+            cursor.execute('commit')
+
+class TestDataUsers:
+    def __init__(self,*args:User):
+        self.__users = args
+
+    def generate(self, number:int, range_age = range(1,100), cursor = None):
+        self.__users = tuple(User(
+                                     username = "".join(random.sample(string.ascii_letters, k = 10)),
+                                     email = f'{"".join(random.sample(string.ascii_letters, k = 10)).lower()}@mail.ru',
+                                     age = random.randint(range_age[0],range_age[-1]),
+                                     cursor = cursor
+                                  ) for i in range(number)
+                            )
+    def cursor(self, cursor):
+        for item in self.__users:
+            item.cursor(other = cursor)
+
+    def save(self):
+        for item in self.__users:
+            if item.cursor:
+                item.save()
+            else:
+                raise ValueError
+    def __getitem__(self,item):
+        return self.__users[item]
+
+if __name__ == '__main__':
+    with SQEnv() as cn:
+        cn.execute_any_sql(sql = 'delete from Users')
+        tests_users = TestDataUsers()
+        tests_users.generate(number=100, range_age=range(1, 100),cursor=cn.cursor)
+        tests_users.save()
+        cn.commit()
+        print(User.avg_age(cursor=cn.cursor))
