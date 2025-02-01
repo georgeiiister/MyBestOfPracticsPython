1406f4d91c279c107e8b185e24f539b6df645603
georgeiiister <georgeiiister@gmail.com>
2025-02-01 17:34:47 +0300

    add new module

---
+++ buble_sorting.py
@@ -0,0 +1,27 @@
+class BS:
+    @staticmethod
+    def sorted(obj):
+        result = []
+        if obj:
+            result.append(obj[0])
+            for i in obj[1:]:
+                for ind, j in enumerate(result):
+                    if i <= j:
+                        result.insert(ind, i)
+                        break
+                else:
+                    result.append(i)
+            return type(obj)(result)
+
+    def __init__(self, obj):
+        self.__obj = obj
+        self.__obj_sorted = None
+
+    def sort(self):
+        self.__obj_sorted = self.__obj_sorted or BS.sorted(obj = self.__obj)
+        return self.__obj_sorted
+
+a = (1,2,3,-190,1000,0,900)
+print(BS.sorted(a))
+
+
