58266f43de1391cfa546d9aae5c69226e38f2e80
georgeiiister <georgeiiister@gmail.com>
2025-03-09 13:49:43 +0300

    add new module

--- threading_.py
+++ threading_.py
@@ -1,32 +1,30 @@
 import threading
 import datetime
 
-a = (i for i in range(10))
-
-
 class A(threading.Thread):
-    def __init__(self, name, obj):
-        super().__init__(name=name)
-        self.__list = []
-        self.__obj = obj
+    def __init__(self, name, orders):
+        super().__init__(name = name)
+        self.__orders = orders
+        self.__result = []
 
     def run(self):
         print(f'start {self.name} {datetime.datetime.now()}')
         while True:
             try:
-                sum((i for i in range(1000_000_0)))
-                self.__list.append(next(self.__obj))
+                sum((i for i in range(10_000_000)))
+                self.__result.append(next(self.__orders))
             except StopIteration:
+                print(f'stop {self.name} {datetime.datetime.now()}')
+                print(self.__result)
                 break
-        print(self.__list)
-
-    @property
-    def lst(self):
-        return self.__list
-
 
 if __name__ == '__main__':
-    t1 = A('thread1', a)
-    t2 = A('thread2', a)
-    t1.start()
-    t2.start()
+
+    in_orders = (i for i in range(10))
+
+    thread1 = A(name='thread1', orders = in_orders)
+    thread2 = A(name='thread2', orders = in_orders)
+
+    thread1.start()
+    thread2.start()
+
