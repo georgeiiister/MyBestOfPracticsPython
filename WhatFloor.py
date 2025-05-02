4ba7f5ef6c57096de44aaf30f9700f9f8fe0f4e4
georgeiiister <georgeiiister@gmail.com>
2025-05-02 22:35:10 +0300

    refactoring

--- WhatFloor.py
+++ WhatFloor.py
@@ -1,14 +1,14 @@
 class WhatFloor:
     @staticmethod
-    def __what_floor(number_appartament:int, on_floor:int):
-        what_floor = (abs(number_appartament)+3)//on_floor
-        return what_floor if number_appartament>=0 else -what_floor
+    def __what_floor(number_apartment:int, on_floor:int):
+        what_floor_ = (abs(number_apartment)+3)//on_floor
+        return what_floor_ if number_apartment>=0 else - what_floor_
 
-    def __init__(self, number_appartament:int, on_floor:int):
-        self.__number_appartament = number_appartament
+    def __init__(self, number_apartment:int, on_floor:int):
+        self.__number_apartment = number_apartment
         self.__on_floor = on_floor
-        self.__what_floor = WhatFloor.__what_floor(number_appartament = number_appartament
-                                       , on_floor = on_floor)
+        self.__what_floor = WhatFloor.__what_floor(number_apartment = self.__number_apartment
+                                       , on_floor = self.__on_floor)
 
     @property
     def what_floor(self):
